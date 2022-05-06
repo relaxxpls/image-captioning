90d9c89ff4a1a9b46ff7deafba1cf5b2a49655d5
Laxman Desai <lolopaw24@gmail.com>
2022-05-06 19:43:25 +0530

    👌 IMPROVE: Improve vocab class and Readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# image-captioning
+# Image Captioning
 
-Image Captioning using CNN + LSTM AutoEncoder Models trained on the Common Objects in Context aka COCO dataset.
+Image Captioning using CNN and LSTM based Encoder Decoder architecture trained on the Common Objects in Context aka COCO dataset.
+
+## Paper
+
+* [Show, Attend and Tell: Neural Image Caption Generation with Visual Attention](https://arxiv.org/abs/1502.03044)
 
 ## Dataset
 
--- vocabulary.py
+++ vocabulary.py
@@ -1,14 +1,17 @@
 from collections import Counter
+from pathlib import Path
+from typing import Dict, List
 
 import spacy
+import torch
 from pycocotools.coco import COCO
 from tqdm import tqdm
 
 
 class Vocabulary:
     def __init__(self):
-        self.word2idx = {}
-        self.idx2word = {}
+        self.word2idx: Dict[str, int] = {}
+        self.idx2word: Dict[int, str] = {}
         self.len = 0
 
         self.pad = self.add_word("<PAD>")
@@ -16,7 +19,7 @@ class Vocabulary:
         self.sos = self.add_word("<SOS>")
         self.eos = self.add_word("<EOS>")
 
-    def add_word(self, word):
+    def add_word(self, word: str) -> int:
         if word not in self.word2idx:
             self.word2idx[word] = self.len
             self.idx2word[self.len] = word
@@ -24,18 +27,18 @@ class Vocabulary:
 
         return self.word2idx[word]
 
-    def __call__(self, word):
+    def __call__(self, word: str) -> int:
         return self.word2idx.get(word, self.unk)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> str:
         return self.idx2word.get(idx, "")
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.len
 
 
 class CocoCaptionsVocabulary(Vocabulary):
-    def __init__(self, annotation_file, freq_threshold=5):
+    def __init__(self, annotation_file: Path, freq_threshold: int = 5):
         super().__init__()
 
         self.coco = COCO(annotation_file)
@@ -43,7 +46,7 @@ class CocoCaptionsVocabulary(Vocabulary):
         self.freq_threshold = freq_threshold
         self.__build_vocab()
 
-    def tokenize(self, text):
+    def tokenize(self, text: str):
         return [tok.text.lower() for tok in self.nlp.tokenizer(text)]
 
     def __build_vocab(self):
@@ -54,8 +57,8 @@ class CocoCaptionsVocabulary(Vocabulary):
             counter.update(tokens)
 
         for word, cnt in counter.items():
+            # ? Add word to vocab if its count >= freq_threshold
             if cnt >= self.freq_threshold:
-                # ? If count of word in dataset is greater than freq_threshold, add it to vocab.
                 self.add_word(word)
 
         print(
@@ -63,13 +66,17 @@ class CocoCaptionsVocabulary(Vocabulary):
             f"{self.len} words of {len(counter)} total."
         )
 
-    def encode(self, text):
+    def encode(self, text: str) -> torch.LongTensor:
         encoded_text = [self(token) for token in self.tokenize(text)]
+        # ? Add SOS and EOS tokens
         encoded_text = [self.sos] + encoded_text + [self.eos]
 
-        return encoded_text
+        return torch.LongTensor(encoded_text)
 
-    def decode(self, encoded_text):
-        text = " ".join([self[idx] for idx in encoded_text[1:-1]])
+    def decode(self, encoded_text: torch.LongTensor) -> str:
+        # ? Remove SOS and EOS tokens
+        encoded_text = encoded_text[1:-1].tolist()
+        tokens = [self[idx] for idx in encoded_text]
+        text = " ".join(tokens)
 
         return text
